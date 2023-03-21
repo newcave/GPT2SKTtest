50752eabbe1125a553026075940d8bebd371446d
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-03-21 20:38:59 +0900

    Update appsktgpt2.py

--- appsktgpt2.py
+++ appsktgpt2.py
@@ -27,12 +27,34 @@ tokenizer = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
 #)
 
 st.title("한국어 텍스트 생성기")
-input_text = st.text_input("시작 텍스트를 입력하세요:")
-generate_button = st.button("생성")
+#input_text = st.text_input("시작 텍스트를 입력하세요:")
+#generate_button = st.button("생성")
 
-if generate_button:
-    if input_text:
-        generated_text = generator(input_text, **generation_args)
-        st.write(generated_text[0]['generated_text'])
-    else:
-        st.write("생성할 텍스트를 입력하세요.")
+#if generate_button:
+#    if input_text:
+#        generated_text = generator(input_text, **generation_args)
+#        st.write(generated_text[0]['generated_text'])
+#    else:
+#        st.write("생성할 텍스트를 입력하세요.")
+        
+text = st.text_area("Input Text:", value=default_text)
+st.write(text)
+punct = ('!', '?', '.')
+
+if text:
+    st.markdown("## Predict")
+    with st.spinner('processing..'):
+        print(f'input > {text}') 
+        input_ids = tokenizer(text)['input_ids']
+        gen_ids = model.generate(torch.tensor([input_ids]),
+                                    max_length=128,
+                                    repetition_penalty=2.0)
+        generated = tokenizer.decode(gen_ids[0,:].tolist()).strip()
+        if generated != '' and generated[-1] not in punct:
+            for i in reversed(range(len(generated))):
+                if generated[i] in punct:
+                    break
+            generated = generated[:(i+1)]
+        print(f'KoGPT > {generated}')
+    st.write(generated)
+        
