79b5534740b4a143388f50d4dd4f5c3492251770
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-03-21 20:44:12 +0900

    Update appsktgpt2.py

--- appsktgpt2.py
+++ appsktgpt2.py
@@ -1,43 +1,41 @@
-import streamlit as st
+import torch
 import string
-from transformers import GPT2LMHeadModel, PreTrainedTokenizerFast, pipeline
+#import streamlit as st
+from transformers import GPT2LMHeadModel, PreTrainedTokenizerFast
 
-# Load model and tokenizer
+
+@st.cache(allow_output_mutation=True)
 def get_model():
     model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
     model.eval()
     return model
 
-# model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
-model = get_model()
 tokenizer = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
-                                                    bos_token='</s>', eos_token='</s>', unk_token='<unk>',
-                                                    pad_token='<pad>', mask_token='<mask>')
+                                                    bos_token='</s>',
+                                                    eos_token='</s>',
+                                                    unk_token='<unk>',
+                                                    pad_token='<pad>',
+                                                    mask_token='<mask>')
 
-# Set up text generation pipeline
-#generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
-#generation_args = dict(
-#    num_beams=4,
-#    repetition_penalty=2.0,
-#    no_repeat_ngram_size=4,
-#    eos_token_id=375,  # \n
-#    max_new_tokens=64,
-#    do_sample=True,
-#    top_k=50,
-#    early_stopping=True
-#)
 
-st.title("한국어 텍스트 생성기")
-#input_text = st.text_input("시작 텍스트를 입력하세요:")
-#generate_button = st.button("생성")
+default_text = "현대인들은 왜 항상 불안해 할까?"
 
-#if generate_button:
-#    if input_text:
-#        generated_text = generator(input_text, **generation_args)
-#        st.write(generated_text[0]['generated_text'])
-#    else:
-#        st.write("생성할 텍스트를 입력하세요.")
-        
+N_SENT = 3
+
+model = get_model()
+st.title("KoGPT2 Demo Page(ver 2.0)")
+
+st.markdown("""
+### 모델
+| Model       |  # of params |   Type   | # of layers  | # of heads | ffn_dim | hidden_dims | 
+|--------------|:----:|:-------:|--------:|--------:|--------:|--------------:|
+| `KoGPT2` |  125M  |  Decoder |   12     | 12      | 3072    | 768 | 
+### 샘플링 방법
+- greedy sampling
+- 최대 출력 길이 : 128/1,024
+## Conditional Generation
+""")
+
 text = st.text_area("Input Text:", value=default_text)
 st.write(text)
 punct = ('!', '?', '.')
@@ -58,4 +56,3 @@ if text:
             generated = generated[:(i+1)]
         print(f'KoGPT > {generated}')
     st.write(generated)
-        
